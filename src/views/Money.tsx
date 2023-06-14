8769c984d126a7668d8cf424a203de1d60052a73
zhouzhaoyuan <787828150@qq.com>
2023-06-14 16:30:19 +0800

    Money.tsx 的TaNotection UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -27,6 +27,26 @@ const TagsSection = styled.section`
   
 `
 const NoteSection = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+    display: flex;
+    align-items: center;
+    >span{
+      margin-right: 16px;
+      white-space: nowrap;
+      
+    }
+    >input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+  
 `
 const CategorySection = styled.section`
 `
@@ -47,7 +67,7 @@ const Money = () => {
       <NoteSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NoteSection>
       <CategorySection>
