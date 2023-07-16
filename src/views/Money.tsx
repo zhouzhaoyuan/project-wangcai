44e816df15be70dda5b0f822de3f65d9445f4bd7
zhouzhaoyuan <787828150@qq.com>
2023-07-16 16:26:59 +0800

    记录提交å数据完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,7 +5,9 @@ import TagsSection from './Money/TagsSection';
 import NoteSection from './Money/NoteSection';
 import CategorySection from './Money/CategorySection';
 import NumberPadSection from './Money/NumberPadSection';
-
+import {Simulate} from 'react-dom/test-utils';
+import submit = Simulate.submit;
+import {useRecords} from '../hooks/useRrcords';
 
 const MyLayout = styled(Page)`
   display: flex;
@@ -13,41 +15,50 @@ const MyLayout = styled(Page)`
 `;
 
 type Category = '-' | '+'
+const defaultFormatData = {
+  tagIds: [] as number[],
+  note: '',
+  category: '-' as Category,
+  amount: 0
+};
 const Money = () => {
+  const [selected, setSelected] = useState(defaultFormatData);
 
-  const [selected, setSelected] = useState({
-    tagIds: [] as number[],
-    note: '',
-    category: '-' as Category,
-    amount: 0
-  });
-
-  const onChange = (obj:Partial<typeof selected>) => {
+  const onChange = (obj: Partial<typeof selected>) => {
     setSelected({
       ...selected,
       ...obj
-    })
-  }
+    });
+  };
+  const {records, addRecord} = useRecords();
+  const submit = () => {
+    if (addRecord(selected)) {
+      alert('保存成功');
+      setSelected(defaultFormatData);
+      console.log(1);
+    }
+  };
 
   return (
 
     <MyLayout>
-      {/*{selected.note}*/}
+      {JSON.stringify(selected)}
 
+      <hr/>
       <TagsSection value={selected.tagIds}
-                   onChange={(tagIds) => onChange({tagIds} )}
+                   onChange={(tagIds) => onChange({tagIds})}
       />
       <NoteSection value={selected.note}
-                   onChange={(note) => {onChange({note})}}
+                   onChange={(note) => {onChange({note});}}
       />
       <CategorySection value={selected.category}
                        onChange={(category) => {
-                         onChange({category})
+                         onChange({category});
                        }}
       />
       <NumberPadSection value={selected.amount}
-                        onChange={(amount) => {onChange({amount})}}
-                        onOk={() => {}}
+                        onChange={(amount) => {onChange({amount});}}
+                        onOk={submit}
       />
     </MyLayout>
   );
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -35,7 +35,7 @@ const Tags = () => {
         {tags.map(tag =>
           <li key={tag.id}>
             <Link to={'/tags/' + tag.id}>
-              <span className="oneLine">{tag.id}{tag.name}</span>
+              <span className="oneLine">{tag.name}</span>
               <Icon name={'right'}></Icon>
             </Link>
           </li>
